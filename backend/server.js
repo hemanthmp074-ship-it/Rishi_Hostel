92b168c3d4b5ea933f7ef86a10e43b9e65d959f6
hemanthmp074-ship-it <hemanthmp074@gmail.com>
2026-01-03 15:10:55 +0530

    minor_initialization_changes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-const express = require('express');
+require('dotenv').config();
 
+const express = require('express');
 const app =express();
+
 require('./src/config/db');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.get('/',(req,res)=>{
